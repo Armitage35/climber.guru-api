d22ae11afd7edb2034ca682337d72dcbcd1e9198
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-09 17:02:41 -0400

    Connect to DB
    Connect to proper DB

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,41 @@
+const pg = require("pg");
+const connectionString = "postgres://postgres:123@localhost:5432/test_db";
+
+
+const config = {
+	user: 'postgres',
+	database: 'climber_guru',
+	password: 'YOURPASSWORD',
+	port: 5432
+};
+
+// pool takes the object above -config- as parameter
+const pool = new pg.Pool(config);
+
 const router = app => {
 	app.get('/', (request, response) => {
 		response.send({
 			message: 'Node.js and Express REST API'
 		});
 	});
+
+
+	app.get('/db', (req, res, next) => {
+		pool.connect(function (err, client, done) {
+			if (err) {
+				console.log("Can not connect to the DB" + err);
+			}
+			client.query('SELECT * FROM student', function (err, result) {
+				done();
+				if (err) {
+					console.log(err);
+					res.status(400).send(err);
+				}
+				res.status(200).send(result.rows);
+			})
+		})
+	});
 }
+
 
 module.exports = router;
