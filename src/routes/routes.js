e2ea098e98c403afd26211595dc8435df959c9c0
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-15 18:21:51 -0400

    Make one route per table

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,13 +17,47 @@ const router = (app) => {
 		});
 	});
 
-	app.get('/users', (req, res, next) => {
+	app.get('/users', (req, res) => {
 		pool.connect((err, client, done) => {
 			if (err) {
 				console.log(`Can not connect to the DB${err}`);
 			}
 
 			client.query('SELECT * FROM account', (err, result) => {
+				done();
+				if (err) {
+					console.log(err);
+					res.status(400).send(err);
+				}
+				res.status(200).send(result.rows);
+			});
+		});
+	});
+
+	app.get('/sessions', (req, res) => {
+		pool.connect((err, client, done) => {
+			if (err) {
+				console.log(`Can not connect to the DB${err}`);
+			}
+
+			client.query('SELECT * FROM session', (err, result) => {
+				done();
+				if (err) {
+					console.log(err);
+					res.status(400).send(err);
+				}
+				res.status(200).send(result.rows);
+			});
+		});
+	});
+
+	app.get('/climbs', (req, res) => {
+		pool.connect((err, client, done) => {
+			if (err) {
+				console.log(`Can not connect to the DB${err}`);
+			}
+
+			client.query('SELECT * FROM session', (err, result) => {
 				done();
 				if (err) {
 					console.log(err);
